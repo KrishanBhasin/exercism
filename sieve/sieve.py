1935346927a6ae65974b487405387c17c22494e6
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2014-12-27 21:50:48 +0300

    cleaned up dictionary creation

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,13 +1,9 @@
 def sieve(num):
-
-	nums_to_test = range(3,num+1,2)
-	primes_dictionary = {}
 	answers = [2]
 	
-	for i in nums_to_test:
-		primes_dictionary[i] = True
+	primes_dictionary = dict.fromkeys(range(3,num+1,2),True)
 	
-	for i in nums_to_test:
+	for i in primes_dictionary:
 		for j in range(3 , (i//2)+2 , 2):
 			if j==i:
 				pass
