8903b38e991a30485e0b7a3660e8ab4506eacd5e
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2015-01-10 11:04:12 +0000

    cleaned up 'series' problem

--- series/series.py
+++ series/series.py
@@ -3,17 +3,16 @@ def slices(numstring,len_answer):
 	
 	if len(list(numstring)) < int(len_answer):
 		raise ValueError("You have asked for more chars than are provided")
-		return
+		
 	elif int(len_answer)==0:
 		raise ValueError("You've asked for a zero length answer")
-		return
+		
 	
-	for i in range(len(list(numstring))+1-len_answer):	#we only want to iterate (n-m) times, to ensure we do not leave the array size
+	for i in range(len(list(numstring))-len_answer+1):	#we only want to iterate (n-m) times, to ensure we do not leave the array size
 
 		answer.append(list(numstring[i:i+len_answer:1]))	#add THIS answer as a list to the answer list
 		
-		
-		for i in range(len(answer[-1])):
-			answer[-1][i] = int(answer[-1][i])	#convert all ansers from string to int
-												#could be done more cleanly....
+		answer[-1] = [int(x) for x in answer[-1]]			#list comprehension to apply the int() function - feels more pythonic than before
+		#map(int,answer[-1])
+
 	return answer
