d68d477f73e06591f6be07ec420aec4909dd0829
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2015-01-20 20:58:07 +0000

    added a 'zip' to hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -5,8 +5,12 @@ def distance(strand1,strand2):
 	
 	hamming_distance = 0
 	
-	for i in range(len(strand1)):
-		if strand1[i]!=strand2[i]:
+	#for i in range(len(strand1)):
+	#	if strand1[i]!=strand2[i]:
+	#		hamming_distance+=1
+			
+	for x,y in zip(strand1, strand2):
+		if x!=y:
 			hamming_distance+=1
 			
 	return hamming_distance
