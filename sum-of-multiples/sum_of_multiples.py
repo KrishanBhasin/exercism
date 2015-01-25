af7546d7fd3cae6ef666612fb17698fb2c68a022
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2015-01-25 13:16:21 +0000

    one line to check if no multiples list is passed to function

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -7,9 +7,12 @@ def check_if_multiple(test_num,list_of_multiples):
 	return 0
 
 def sum_of_multiples(number, multiples_list = None):
-	if multiples_list == None:
-		multiples_list = [3,5]
+	multiples_list = multiples_list or [3,5]
+	#implicitly check if None is passed to the function
+	
+	
 	return sum(list(filter(lambda x: check_if_multiple(x,multiples_list),range(1,number))))
+	
 	#the above line:
 		#uses a lambda to call check_if_multiple multiple times with different values of x, fixing the mutliples list
 		#filters out any returns that are false (returned zero)
