57e6626eebf64b3d336147ba20d9db89339f52a2
Krishan <krishan.bhasin42+github@gmail.com>
2015-05-13 17:39:36 +0200

    simpler thanks to #maths!

--- grains/grains.py
+++ grains/grains.py
@@ -1,11 +1,6 @@
-from math import pow
-
 def on_square(num):
-	return int(pow(2,num-1))
+	return 2**(num-1)
 
 def total_after(num):
-	sum = 0
-	for i in range(1,num+1):
-		sum += on_square(i)
-		
-	return sum
+	#maths!
+	return 2**num -1
